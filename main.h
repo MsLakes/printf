bf976f1e4c9a5ca29e67b0435828599a94e8e071
obengben <obengbenjamin02@gmail.com>
2023-09-25 21:03:07 -0700

    Update

--- main.h
+++ main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+int binary (int num);
+int octal(int num);
 void print_char(char c);
 void print_string(char *str);
 
